43df84ba45f8562fa543d4073068977ab0e88614
Appleblue17 <hluo262@connect.hkust-gz.edu.cn>
2025-07-20 02:13:38 +0800

    [feat] Add  Markdown file viewer.
    - Added a Markdown viewer that supports math rendering and image links.
    - Integrated KaTeX for rendering mathematical expressions in Markdown.
    - Configured TypeScript paths for public assets.

--- src/app/view/contests/[contest]/problems/[problem]/[file]/file-viewer.tsx
+++ src/app/view/contests/[contest]/problems/[problem]/[file]/file-viewer.tsx
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import FileViewerPDFWrapper from "./file-viewer-pdf-wrapper";
 import FileViewerSource from "./file-viewer-source";
+import FileViewerMarkdownWrapper from "./file-viewer-markdown-wrapper";
 
 export default function FileViewer({
   contest,
@@ -17,11 +18,16 @@ export default function FileViewer({
 
   if (fs.existsSync(filePath)) {
     if (ext === ".pdf") {
+      // For PDF files, use the PDF viewer component
       const pdfPath = path.join("/contests", contest, "problems", problem, file);
-
       return <FileViewerPDFWrapper pdfPath={pdfPath} />;
     }
     if (ext === ".md") {
+      // For Markdown files, use remark to convert to HTML
+      const dirPath = path.join("http://localhost:3000/", "contests", contest, "problems", problem);
+      return (
+        <FileViewerMarkdownWrapper dirPath={dirPath} raw={fs.readFileSync(filePath, "utf-8")} />
+      );
     } else {
       // Source code or other file types can be handled here
       return <FileViewerSource code={fs.readFileSync(filePath, "utf-8")} ext={ext} />;
